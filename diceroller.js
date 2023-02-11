d239293c9ab45e4890a552eecf97324a8495ac07
Tomo <78112242+Tomokaari@users.noreply.github.com>
2023-02-11 00:14:32 -0500

    update + modifier

--- diceroller.js
+++ diceroller.js
@@ -3,6 +3,7 @@ const d6Button = document.querySelector('#d6');
 const d8Button = document.querySelector('#d8');
 const d10Button = document.querySelector('#d10');
 const d20Button = document.querySelector('#d20');
+const modifier = document.querySelector('#modifier');
 const result = document.querySelector('#result');
 
 function rollDie(numSides) {
@@ -10,21 +11,26 @@ function rollDie(numSides) {
 }
 
 d4Button.addEventListener('click', () => {
-  result.textContent = `You rolled a D4 and got a ${rollDie(4)}`;
+  const roll = rollDie(4) + Number(modifier.value);
+  result.textContent = `You rolled a D4, got a ${rollDie(4)}, and with a modifier of ${modifier.value} got a ${roll}`;
 });
 
 d6Button.addEventListener('click', () => {
-  result.textContent = `You rolled a D6 and got a ${rollDie(6)}`;
+  const roll = rollDie(6) + Number(modifier.value);
+  result.textContent = `You rolled a D6, got a ${rollDie(6)}, and with a modifier of ${modifier.value} got a ${roll}`;
 });
 
 d8Button.addEventListener('click', () => {
-  result.textContent = `You rolled a D8 and got a ${rollDie(8)}`;
+  const roll = rollDie(8) + Number(modifier.value);
+  result.textContent = `You rolled a D8, got a ${rollDie(8)}, and with a modifier of ${modifier.value} got a ${roll}`;
 });
 
 d10Button.addEventListener('click', () => {
-  result.textContent = `You rolled a D10 and got a ${rollDie(10)}`;
+  const roll = rollDie(10) + Number(modifier.value);
+  result.textContent = `You rolled a D10, got a ${rollDie(10)}, and with a modifier of ${modifier.value} got a ${roll}`;
 });
 
 d20Button.addEventListener('click', () => {
-  result.textContent = `You rolled a D20 and got a ${rollDie(20)}`;
+  const roll = rollDie(20) + Number(modifier.value);
+  result.textContent = `You rolled a D20, got a ${rollDie(20)}, and with a modifier of ${modifier.value} got a ${roll}`;
 });
