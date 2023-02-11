41d9f6f43a1dd8b855eb30748c802c1976b0b59a
Tomo <78112242+Tomokaari@users.noreply.github.com>
2023-02-11 00:43:40 -0500

    gif update

--- diceroller.js
+++ diceroller.js
@@ -3,34 +3,38 @@ const d6Button = document.querySelector('#d6');
 const d8Button = document.querySelector('#d8');
 const d10Button = document.querySelector('#d10');
 const d20Button = document.querySelector('#d20');
-const modifier = document.querySelector('#modifier');
 const result = document.querySelector('#result');
+const diceGif = document.querySelector('#diceGif');
 
 function rollDie(numSides) {
   return Math.floor(Math.random() * numSides) + 1;
 }
 
 d4Button.addEventListener('click', () => {
-  const roll = rollDie(4) + Number(modifier.value);
-  result.textContent = `You rolled a D4, got a ${rollDie(4)}, and with a modifier of ${modifier.value} got a ${roll}`;
+  result.textContent = `You rolled a D4 and got a ${rollDie(4)}`;
+  diceGif.src = "d4.gif";
+  diceGif.style.display = "block";
 });
 
 d6Button.addEventListener('click', () => {
-  const roll = rollDie(6) + Number(modifier.value);
-  result.textContent = `You rolled a D6, got a ${rollDie(6)}, and with a modifier of ${modifier.value} got a ${roll}`;
+  result.textContent = `You rolled a D6 and got a ${rollDie(6)}`;
+  diceGif.src = "d6.gif";
+  diceGif.style.display = "block";
 });
 
 d8Button.addEventListener('click', () => {
-  const roll = rollDie(8) + Number(modifier.value);
-  result.textContent = `You rolled a D8, got a ${rollDie(8)}, and with a modifier of ${modifier.value} got a ${roll}`;
+  result.textContent = `You rolled a D8 and got a ${rollDie(8)}`;
+  diceGif.src = "d8.gif";
+  diceGif.style.display
 });
 
 d10Button.addEventListener('click', () => {
-  const roll = rollDie(10) + Number(modifier.value);
-  result.textContent = `You rolled a D10, got a ${rollDie(10)}, and with a modifier of ${modifier.value} got a ${roll}`;
-});
+  result.textContent = `You rolled a D10 and got a ${rollDie(10)}`;
+  diceGif.src = "d10.gif";
+  diceGif.style.display
+  });
 
 d20Button.addEventListener('click', () => {
-  const roll = rollDie(20) + Number(modifier.value);
-  result.textContent = `You rolled a D20, got a ${rollDie(20)}, and with a modifier of ${modifier.value} got a ${roll}`;
-});
+  result.textContent = `You rolled a D20 and got a ${rollDie(20)}`;
+  diceGif.src = "d20.gif";
+  diceGif.style.display
